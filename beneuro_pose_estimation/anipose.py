9fb8ebeb99237d661b3ba84b2acaecdc1e45a6fc
Martin Esparza <martinesparza22.me@gmail.com>
2024-09-25 16:30:43 +0100

    Organizing modules and building more boilerplate

--- beneuro_pose_estimation/anipose.py
+++ beneuro_pose_estimation/anipose.py
@@ -3,9 +3,24 @@ Module to carry out anipose operations
 """
 
 
-def to_3d(session_name: str) -> None:
+def compute_3d_predictions():
+    return
+
+
+def save_to_csv() -> None:
+    return
+
+
+def pose_estimation(session_name: str) -> None:
     """
     Main conversion routing from videos to 2d keypoints and angles
+
+    Steps:
+
+    1. Access videos from session
+    2. Load Sleap sleap (previously trained) and compute 2d prediction
+    3. Use 2d predictions to compute 3D predictions
+    3. Save to a csv.
 
 
     Parameters
---
+++ beneuro_pose_estimation/sleap/sleap.py
@@ -0,0 +1,14 @@
+"""
+Module for Sleap processing
+"""
+
+
+def compute_2d_predictions():
+    """
+    Create or load camera-wise sleap and compute the 2 predictions of each one
+
+    Returns
+    -------
+
+    """
+    return
